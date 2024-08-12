e2e9757e6669bce42c5ce48a91e171be2f979fbf
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-08-12 14:18:12 +0300

    FlashBorrower and FlashLender interfaces

--- contracts/IERC3156FlashBorrower.sol
+++ contracts/IERC3156FlashBorrower.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.24;
 
 interface IERC3156FlashBorrower {
-    
+    function onFlashLoan (
+        address initiator, 
+        address token, 
+        uint256 amount, 
+        uint256 fee,
+        bytes calldata data
+    ) external returns(bytes32);
 }
--- contracts/IERC3156FlashLender.sol
+++ contracts/IERC3156FlashLender.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "./IERC3156FlashBorrower.sol";
+
 interface IERC3156FlashLender {
-    
+    function flashFee( address token, uint256 amount) external view returns(uint256);
+
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token, 
+        uint256 amount, 
+        bytes calldata data
+    ) external returns(bool);
 }
